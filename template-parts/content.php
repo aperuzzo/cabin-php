3f4843d5fedd4cb4b1f856a4202fe7e63b902450
Tony Peruzzo <anthony_peruzzo@hotmail.com>
2016-02-16 15:28:22 -0600

    removing comments and date info from posts

--- template-parts/content.php
+++ template-parts/content.php
@@ -19,9 +19,7 @@
 			}
 
 		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php cabin_php_posted_on(); ?>
-		</div><!-- .entry-meta -->
+		
 		<?php
 		endif; ?>
 	</header><!-- .entry-header -->
@@ -41,7 +39,5 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php cabin_php_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+	
 </article><!-- #post-## -->
