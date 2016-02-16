2d055ec6c186750aae726febbe1a6425480abc21
Tony Peruzzo <anthony_peruzzo@hotmail.com>
2016-02-16 12:11:56 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+cabin_php is a theme created for MCAD's php/wordpress course
+
+This theme started as a foundation from _s (underscores): http://underscores.me/
---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package cabin_php
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cabin_php' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'cabin_php' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'cabin_php' ), 'cabin_php', '<a href="http://anthonyperuzzo.com" rel="designer">Anthony Peruzzo</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
